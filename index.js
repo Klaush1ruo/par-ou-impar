8d555f479cca50915f3c123a6c9e0070f3411d49
Klaush1ruo <klausosubzero@gmail.com>
2024-06-19 11:12:15 -0300

    minhas alterações dia 19

--- index.js
+++ index.js
@@ -1,22 +1,23 @@
 function playGame() {
-    var playerChoice = parseInt(document.getElementById('playerChoice').value);
+    var player1Choice = parseInt(document.getElementById('player1Choice').value);
 
-    if (isNaN(playerChoice) || playerChoice < 0 || playerChoice > 10) {
-        alert('Por favor, insira um número entre 0 e 10.');
+    if (isNaN(player1Choice) || player1Choice < 0 || player1Choice > 10) {
+        alert('Por favor, jogador 1, insira um número entre 0 e 10.');
         return;
     }
 
     var robotChoice = Math.floor(Math.random() * 11); // Número aleatório entre 0 e 10 para o robô
 
-    var total = playerChoice + robotChoice;
+    var total = player1Choice + robotChoice;
     var isEven = total % 2 === 0;
 
-    var resultMessage = `Você escolheu ${playerChoice}. O robô escolheu ${robotChoice}.<br>`;
+    var resultMessage = `<imsrc="avatar1.png" > Jogador 1 escolheu ${player1Choice}.<br>`;
+    resultMessage += `<imsrc="avatar2.png" > Jogador 2 (robô) escolheu ${robotChoice}.<br>`;
     
     if (isEven) {
-        resultMessage += `Total é ${total}. Par! Você ganhou.`;
+        resultMessage += `Total é ${total}. Par! Jogador 1 ganhou.`;
     } else {
-        resultMessage += `Total é ${total}. Ímpar! O robô ganhou.`;
+        resultMessage += `Total é ${total}. Ímpar! Jogador 2 (robô) ganhou.`;
     }
 
     document.getElementById('result').innerHTML = resultMessage;
